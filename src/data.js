b18bd590f1f18559335a190a3a9aabe78ac97b0d
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-07-18 15:04:13 -0300

    added project myShopOnline

--- src/data.js
+++ src/data.js
@@ -1,44 +1,36 @@
 //  icons
-import {
-  FiGithub,
-  FiLinkedin,
-  FiMail,
-  FiMapPin,
-} from 'react-icons/fi';
-
-
+import { FiGithub, FiLinkedin, FiMail, FiMapPin } from "react-icons/fi";
 
 // projects images
-import Project1 from './assets/img/projects/p1.png';
-import Project2 from './assets/img/projects/p2.png';
+import Project1 from "./assets/img/projects/p1.png";
+import Project2 from "./assets/img/projects/p2.png";
+import Project3 from "./assets/img/projects/p3.png";
 
-
-import SkillImg1 from './assets/img/skills/html5.png';
-import SkillImg2 from './assets/img/skills/CSS.png';
-import SkillImg3 from './assets/img/skills/js.png';
-import SkillImg4 from './assets/img/skills/reactjs.png';
-import SkillImg5 from './assets/img/skills/redux.png';
-import SkillImg6 from './assets/img/skills/nodejs.png';
-import SkillImg7 from './assets/img/skills/git.png';
-import SkillImg8 from './assets/img/skills/postgresql.png';
-
+import SkillImg1 from "./assets/img/skills/html5.png";
+import SkillImg2 from "./assets/img/skills/CSS.png";
+import SkillImg3 from "./assets/img/skills/js.png";
+import SkillImg4 from "./assets/img/skills/reactjs.png";
+import SkillImg5 from "./assets/img/skills/redux.png";
+import SkillImg6 from "./assets/img/skills/nodejs.png";
+import SkillImg7 from "./assets/img/skills/git.png";
+import SkillImg8 from "./assets/img/skills/postgresql.png";
 
 export const navigation = [
   {
-    name: 'home',
-    href: 'home',
+    name: "home",
+    href: "home",
   },
   {
-    name: 'about',
-    href: 'about',
+    name: "about",
+    href: "about",
   },
   {
-    name: 'portfolio',
-    href: 'portfolio',
+    name: "portfolio",
+    href: "portfolio",
   },
   {
-    name: 'contact',
-    href: 'contact',
+    name: "contact",
+    href: "contact",
   },
 ];
 
@@ -46,62 +38,62 @@ export const navigation = [
 export const social = [
   {
     icon: <FiLinkedin />,
-    href: 'https://www.linkedin.com/in/kimberly-blandon/?locale=en_US',
+    href: "https://www.linkedin.com/in/kimberly-blandon/?locale=en_US",
   },
   {
     icon: <FiGithub />,
-    href: 'https://github.com/Kimblag',
+    href: "https://github.com/Kimblag",
   },
 ];
-
 
 // projects
 export const projectsData = [
   {
-    id: '1',
+    id: "1",
     image: Project1,
-    name: 'Countries App',
-    category: 'Full Stack',
-    href: 'https://countries-main-app.vercel.app/',
-    description: 'A full stack application that allows users to search for countries and view their information.',
+    name: "Countries App",
+    category: "Full Stack",
+    href: "https://countries-main-app.vercel.app/",
+    description:
+      "A full stack application that allows users to search for countries and view their information.",
   },
   {
-    id: '2',
+    id: "2",
     image: Project2,
-    name: 'Shoes e-commerce',
-    category: 'Full Stack',
-    href: 'https://e-commerce-five-mu.vercel.app/',
-    description: 'This PERN application allows users to create an account, login, and view their cart. It is based on the Final Project that my work group and I made for Henry bootcamp. I wanted to improve my frontend skills by redoing it.',
+    name: "Shoes e-commerce",
+    category: "Full Stack",
+    href: "https://e-commerce-five-mu.vercel.app/",
+    description:
+      "This PERN application allows users to create an account, login, and view their cart. It is based on the Final Project that my work group and I made for Henry bootcamp. I wanted to improve my frontend skills by redoing it.",
   },
-  // {
-  //   id: '3',
-  //   image: Project3,
-  //   name: 'project name 3',
-  //   category: 'Backend',
-  // },
-  // {
- 
+  {
+    id: "3",
+    image: Project3,
+    name: "project name 3",
+    category: "Full stack",
+    href: "https://my-shop-online.vercel.app/",
+    description:
+      "This application is an ecommerce in which user can register, login, search products, add products to favorites / cart, recover passwords, verify user account. It was made it with TypeScript, Node.js, Express, MongoDB, React, Redux Toolkit",
+  },
 ];
-
 
 export const projectsNav = [
   {
-    name: 'all',
+    name: "all",
   },
   {
-    name: 'Full Stack',
+    name: "Full Stack",
   },
   {
-    name: 'Frontend',
+    name: "Frontend",
   },
   {
-    name: 'Backend',
+    name: "Backend",
   },
   // {
   //   name: 'Mobile Development',
   // },
 ];
-
 
 export const skills = [
   {
@@ -130,19 +122,17 @@ export const skills = [
   },
 ];
 
-
-
 export const contact = [
   {
     icon: <FiMail />,
-    title: 'Have a question?',
-    subtitle: 'I am here to help you.',
-    description: 'Email me at kimberly.blandong@gmail.com',
+    title: "Have a question?",
+    subtitle: "I am here to help you.",
+    description: "Email me at kimberly.blandong@gmail.com",
   },
   {
     icon: <FiMapPin />,
-    title: 'Current Location',
-    subtitle: 'Buenos Aires, Argentina',
-    description: 'Serving clients worldwide',
+    title: "Current Location",
+    subtitle: "Buenos Aires, Argentina",
+    description: "Serving clients worldwide",
   },
 ];
